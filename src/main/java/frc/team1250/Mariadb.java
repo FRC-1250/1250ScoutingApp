d8923a5a561f9ecaf45e3e760d43f744d7067505
Rodney-Lewis <rodneyll@umich.edu>
2018-10-07 16:23:41 -0400

    Introduced mariadb
    Mariadb functions to connect, query, and update
    Using JBDC 2.3.0 added to gradle

---
+++ src/main/java/frc/team1250/Mariadb.java
@@ -0,0 +1,92 @@
+package frc.team1250;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class Mariadb {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    static final String DB_URL = "jdbc:mariadb://127.0.0.1:3306/frc";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+    
+    private static Connection GetConnection() throws SQLException {
+		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    	return conn;
+    }
+    
+    private static void CloseConnection(Connection conn) {
+		try {
+			conn.close();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private static void CloseStatement(Statement stmt) {
+		try {
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private static void CloseResultSet(ResultSet rs) {
+		try {
+			rs.close();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static ResultSet Query(String sqlQuery) {
+		Connection queryConnection = null;
+		Statement queryStatement = null;
+		ResultSet queryResults = null;
+		
+		try {
+			queryConnection = GetConnection();
+			queryStatement = queryConnection.createStatement();
+			queryResults = queryStatement.executeQuery(sqlQuery);
+			return queryResults;
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			CloseConnection(queryConnection);
+			CloseStatement(queryStatement);
+			//CloseResultSet(queryResults);
+		}
+		return queryResults;
+	}
+	
+	public static int Update(String sqlQuery) {
+		int effectedRows = 0;
+		Connection updateConnection = null;
+		Statement updateStatement = null;
+		
+		try {
+			updateConnection = GetConnection();
+			updateStatement = updateConnection.createStatement();
+			effectedRows = updateStatement.executeUpdate(sqlQuery);
+			return effectedRows;
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			CloseConnection(updateConnection);
+			CloseStatement(updateStatement);
+		}
+		return effectedRows;
+	}
+}
